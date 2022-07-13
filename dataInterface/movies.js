b3ef0a9ba7dce174eb23f7b7a18d9a172f4e185e
CheriMarie <cherimarie@gmail.com>
2022-07-12 18:41:44 -0700

    Add movies endpoints

---
+++ dataInterface/movies.js
@@ -0,0 +1,22 @@
+
+module.exports = {}
+
+module.exports.getAll = () => {
+  return []
+}
+
+module.exports.getById = (movieId) => {
+  return {}
+}
+
+module.exports.deleteById = (movieId) => {
+  return []
+}
+
+module.exports.updateById = (movieId, newObj) => {
+  return []
+}
+
+module.exports.create = (newObj) => {
+  return {}
+}
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/items", require("./items"));
+router.use("/movies", require("./movies"));
 
 module.exports = router;
